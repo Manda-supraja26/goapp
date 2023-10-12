ba84cf9d8cc910d0cfa3c4aa3b2c2203418a1187
Manda-supraja26 <mandasupraja1365139@gmail.com>
2023-10-13 01:22:21 +0530

    route update

--- pkg/routes/movieroutes.go
+++ pkg/routes/movieroutes.go
@@ -1,11 +1,17 @@
 package routes
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/Manda-supraja26/moviestore/pkg/controllers"
 	"github.com/gorilla/mux"
 )
 
 var RegisterMovieRoutes = func(router *mux.Router) {
+	router.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, welcome")
+	}))
 	router.HandleFunc("/movies", controllers.GetAllMovies).Methods("GET")
 	router.HandleFunc("/movie/{movieid}", controllers.GetMovieByID).Methods("Get")
 	router.HandleFunc("/movie", controllers.CreateMovie).Methods("POST")
